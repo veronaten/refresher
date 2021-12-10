1c41b27f1ea1ffd13c8f72d0836c0db9ff6c227b
Veronika <verona.binko@gmail.com>
2021-12-11 01:30:19 +0300

    Feat: connect with database firebase

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,3 +1,39 @@
+// import { useDispatch } from 'react-redux';
+
+// import { cartActions } from '../../store/cart-slice';
+// import Card from '../UI/Card';
+// import classes from './ProductItem.module.css';
+
+// const ProductItem = (props) => {
+//   const dispatch = useDispatch();
+//   const { title, price, description, id } = props;
+
+//   const addToCartHandler = () => {
+//     dispatch(cartActions.addItemToCart({
+//       id,
+//       title,
+//       price
+//     }));
+//   };
+
+//   return (
+//     <li className={classes.item}>
+//       <Card>
+//         <header>
+//           <h3>{title}</h3>
+//           <div className={classes.price}>${price.toFixed(2)}</div>
+//         </header>
+//         <p>{description}</p>
+//         <div className={classes.actions}>
+//           <button onClick={addToCartHandler}>Add to Cart</button>
+//         </div>
+//       </Card>
+//     </li>
+//   );
+// };
+
+// export default ProductItem;
+
 import { useDispatch } from 'react-redux';
 
 import { cartActions } from '../../store/cart-slice';
@@ -6,14 +42,20 @@ import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id,
-      title,
-      price
-    }));
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,15 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
-    totalQuantity: 0
+    totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
@@ -17,7 +21,7 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
@@ -26,16 +30,15 @@ const cartSlice = createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find(item => item.id === id);
+      const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
-    }
-  }
+    },
+  },
 });
 
 export const cartActions = cartSlice.actions;
